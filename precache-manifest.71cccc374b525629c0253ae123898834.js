cc185cc25c77f34f8cb7a0a1b81a5215ad5eb43d
TomasiakM <marcintomasiak00@gmail.com>
2020-11-09 17:58:24 +0100

    Updates

---
+++ precache-manifest.71cccc374b525629c0253ae123898834.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c72caa84695d31327e9a8e88b99f5350",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "a14c940c041aae666b59",
+    "url": "/shop/static/css/main.c90f332d.chunk.css"
+  },
+  {
+    "revision": "4665f64e86d3e592c7e8",
+    "url": "/shop/static/js/2.9bbb70be.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/shop/static/js/2.9bbb70be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a14c940c041aae666b59",
+    "url": "/shop/static/js/main.335b45c9.chunk.js"
+  },
+  {
+    "revision": "aa1945b71dc1f6fb53f2",
+    "url": "/shop/static/js/runtime-main.4a8e269c.js"
+  },
+  {
+    "revision": "a860f7880edd1ef248f47efe6c747a53",
+    "url": "/shop/static/media/3365-front.a860f788.jpg"
+  },
+  {
+    "revision": "555504c47db5358aad6991f913ca80b6",
+    "url": "/shop/static/media/b450-gaming-plus-max.555504c4.png"
+  },
+  {
+    "revision": "932e8cc620f86d58f3ee7536568e3846",
+    "url": "/shop/static/media/b450-tomahawk.932e8cc6.png"
+  },
+  {
+    "revision": "6e5154f18f3c2e4d47b983ea0a98cd05",
+    "url": "/shop/static/media/b460-hd3.6e5154f1.png"
+  },
+  {
+    "revision": "1014340710813ef8585c041daef6b69f",
+    "url": "/shop/static/media/gtx-1660.10143407.png"
+  },
+  {
+    "revision": "6a456ae82993236e280f65583dd7af57",
+    "url": "/shop/static/media/i5-10400.6a456ae8.png"
+  },
+  {
+    "revision": "be218e739dd4a26782b58a6a1361812d",
+    "url": "/shop/static/media/i7-10700.be218e73.png"
+  },
+  {
+    "revision": "6e89dd0748dd0110d40cf4679a06a7ce",
+    "url": "/shop/static/media/ram.6e89dd07.png"
+  },
+  {
+    "revision": "3d14f526d965f4feafb90ca4f8f16eac",
+    "url": "/shop/static/media/rtx-2060.3d14f526.png"
+  },
+  {
+    "revision": "26898283b1adadacf6e12f021edf28a0",
+    "url": "/shop/static/media/rtx-2070.26898283.png"
+  },
+  {
+    "revision": "740c3f63187d70c7e632f7d8b11be66e",
+    "url": "/shop/static/media/rtx-2080.740c3f63.png"
+  },
+  {
+    "revision": "8bdb753817a31ce3d8d75e096f8cadd4",
+    "url": "/shop/static/media/rx-5600-xt.8bdb7538.png"
+  },
+  {
+    "revision": "5c41885266a41283fcbb7a4191009879",
+    "url": "/shop/static/media/rx-5700-xt.5c418852.png"
+  },
+  {
+    "revision": "43d02b999a6d3923a6e78350197d4f6f",
+    "url": "/shop/static/media/rx-580.43d02b99.png"
+  },
+  {
+    "revision": "b807bacf184cb5f2051b6997b8a86005",
+    "url": "/shop/static/media/ryzen-5-3600.b807bacf.png"
+  },
+  {
+    "revision": "bd30e919c38d0391ac2b862ab3f79f6f",
+    "url": "/shop/static/media/ryzen-7-3700-x.bd30e919.png"
+  },
+  {
+    "revision": "20bd2715b788e98b840b37ce6f2b09d6",
+    "url": "/shop/static/media/z490-gaming-plus.20bd2715.png"
+  }
+]);
